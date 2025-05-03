2e0ea4f82c6b84e97afa09a162976d0139904ac0
Lakshmi P Mantravadi <prasanthiml@gmail.com>
2025-05-03 15:43:43 -0500

    Remove build and docs folder from repo and add to .gitignore

--- docs/components/Gameboard.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import "../index.css";
-export default function GameBoard({ onSelectSquare, gameBoard, }) {
-    return (_jsx("ol", { id: "game-board", children: gameBoard.map((row, rowIndex) => (_jsx("li", { children: _jsx("ol", { children: row.map((playerSymbol, colIndex) => (_jsx("li", { children: _jsx("button", { disabled: playerSymbol != null, onClick: () => onSelectSquare(rowIndex, colIndex), children: playerSymbol }) }, colIndex))) }) }, rowIndex))) }));
-}
